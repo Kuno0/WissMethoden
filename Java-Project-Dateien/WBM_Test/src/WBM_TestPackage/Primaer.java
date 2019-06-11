2b6c9897413814b7cde9ec9d79656b15ce2ab54a
NurJanAtGit <51383667+NurJanAtGit@users.noreply.github.com>
2019-06-11 22:44:07 +0200

    Buttons, Dropdown usw. mit Rechtsklick Start-/Zielauswahl
    - Erweiterung mit Buttons, Dropdowns und Textfeldern zur Bedienung
    - Auswahl des nächstgelegenen Punktes als Start oder Ziel durch Rechtsklick auf Karte möglich und fertig für Weitergabe an Prolog
    - Dropdown Auswahl der Attribute und anschließende Neuzeichnung über Button möglich

--- Java-Project-Dateien/WBM_Test/src/WBM_TestPackage/Primaer.java
+++ Java-Project-Dateien/WBM_Test/src/WBM_TestPackage/Primaer.java
@@ -1,6 +1,10 @@
 package WBM_TestPackage;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,56 +13,172 @@ import java.util.Arrays;
 
 import javax.swing.*;
 
-public class Primaer
+public class Primaer extends JFrame implements ActionListener
 {
-	public static Pfad[] Sammlung;
+	private static final long serialVersionUID = 6930173412792515849L;
 	
-	public static void main(String[] args)
-	{		
-		JFrame fenster = new JFrame();
-		//fenster.setExtendedState(Frame.MAXIMIZED_BOTH);
-		fenster.setLocationRelativeTo(null);
-		fenster.setTitle("Navigationskarte");
-		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	static Pfad[] Sammlung;
+	JButton buttonSetStart;
+	JButton buttonSetEnd;
+	JButton buttonGetWay;
+	@SuppressWarnings("rawtypes")
+	JComboBox comboBoxAttributwahl;
+	JTextField textFStart;
+	JTextField textFZiel;
+	
+	public static int spalte_NamePStart=0;
+	public static int spalte_NamePEnd=1;
+	public static int spalte_StartPX=3;
+	public static int spalte_StartPY=4;
+	public static int spalte_EndPX=5;
+	public static int spalte_EndPY=6;
+	public static int spalte_MerkmalBarrierefrei=8;
+	public static String csvDatenbasis = ".\\DatenBasis.CSV";
+	
+	public static Primaer p;
+	JPopupMenu popup;
+	
+	public Integer MenuXPos;
+	public Integer MenuYPos;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Primaer()
+	{
+		this.setTitle("Navigationskarte");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//pfadObjekteLaden("Ohne");
-		pfadObjekteLaden("Barrierefrei");
-		
+// JLabel mit Hintergrundbild erzeugen
 		ImageIcon verwendetesBild = new ImageIcon(".\\Karte.png");
-		JLabel kartenLabel = new JLabel(verwendetesBild, JLabel.LEFT);
-		kartenLabel.setOpaque(true);		
+		JLabel hintergrundbildLabel = new JLabel(verwendetesBild, JLabel.LEFT);
+		hintergrundbildLabel.setOpaque(true);
 		
-		Drawing linienUndPunkte = new Drawing();
-		linienUndPunkte.WertUebergeben(Sammlung);
+		popup = new JPopupMenu();
+		ActionListener menuListener = new ActionListener()
+	    {
+	      public void actionPerformed(ActionEvent event)
+	      {
+	 		 if(event.getActionCommand()=="Punkt als Start festlegen")
+			 {
+	 			 textFStart.setText("Nächster: "+ naechstenPunktErmitteln(MenuXPos,MenuYPos) +"; X: "+ MenuXPos +"; Y: "+ MenuYPos);
+			 }
+	 		 else if (event.getActionCommand()=="Punkt als Ziel festlegen")
+	 		 {
+	 			textFZiel.setText("Nächster: "+ naechstenPunktErmitteln(MenuXPos,MenuYPos) +"; X: "+ MenuXPos +"; Y: "+ MenuYPos);
+	 		 }
+	      }
+	    };
+	    
+	    JMenuItem itemStart;
+	    JMenuItem itemZiel;
+	    popup.add(itemStart = new JMenuItem("Punkt als Start festlegen"));
+	    itemStart.addActionListener(menuListener);
+	    popup.add(itemZiel = new JMenuItem("Punkt als Ziel festlegen"));
+	    itemZiel.addActionListener(menuListener);
+	    popup.setLightWeightPopupEnabled(false);
 		
+		
+// Buttons erstellen
+	    
+		JPanel buttonPanel = new JPanel();
+		buttonSetStart = new JButton("Test1");
+		buttonSetEnd = new JButton("Test2");
+		buttonGetWay = new JButton("Strecke ermitteln");
+		
+		String[] attributauswahl = { "Ohne", "Barrierefrei" };
+		comboBoxAttributwahl = new JComboBox(attributauswahl);
+		
+		textFStart = new JTextField("Test Start");
+		textFZiel = new JTextField("Test Ziel");
+		
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));	
+		buttonPanel.add(buttonSetStart);
+		buttonSetStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+		buttonPanel.add(Box.createRigidArea(new Dimension(0,25)));
+		buttonPanel.add(buttonSetEnd);
+		buttonSetEnd.setAlignmentX(Component.CENTER_ALIGNMENT);
+		buttonPanel.add(Box.createRigidArea(new Dimension(0,25)));
+		buttonPanel.add(buttonGetWay);
+		buttonGetWay.setAlignmentX(Component.CENTER_ALIGNMENT);
+		buttonPanel.add(Box.createRigidArea(new Dimension(0,25)));
+		buttonPanel.add(comboBoxAttributwahl);
+		comboBoxAttributwahl.setAlignmentX(Component.CENTER_ALIGNMENT);
+		buttonPanel.add(Box.createRigidArea(new Dimension(0,25)));
+		buttonPanel.add(textFStart);
+		textFStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+		buttonPanel.add(Box.createRigidArea(new Dimension(0,25)));
+		buttonPanel.add(textFZiel);
+		textFZiel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		buttonSetStart.addActionListener(this);
+		buttonSetEnd.addActionListener(this);
+		buttonGetWay.addActionListener(this);
+		
+		//addMouseListener(new ClickListener());
+		
+// Flowlayout erstellen
 		FlowLayout LayoutManagement = new FlowLayout();
 		LayoutManagement.setHgap(0);
 		LayoutManagement.setVgap(0);
 		LayoutManagement.setAlignment(FlowLayout.LEFT);
 		
-		Container contentpaneKarte = fenster.getContentPane();
+// Container vorbereiten		
+		Container contentpaneKarte = this.getContentPane();
 		contentpaneKarte.setLayout(LayoutManagement);
-		contentpaneKarte.add(kartenLabel);
-
-		fenster.setGlassPane(linienUndPunkte);
-		linienUndPunkte.setVisible(true);
-
-		fenster.pack();
-		fenster.setSize(1600, 920);
-		fenster.setLocation(120,60);
-		fenster.setVisible(true);
+		contentpaneKarte.add(hintergrundbildLabel);
+		contentpaneKarte.add(buttonPanel);
+		hintergrundbildLabel.addMouseListener(new ClickListener());
+		
+		this.setLocationRelativeTo(null);
+		this.setSize(1700, 908);
+		//this.setExtendedState(Frame.MAXIMIZED_BOTH);
+		this.setLocation(120,60);
 	}
 
+	public static void main(String[] args)
+	{	
+// Array mit Pfaden erstellen
+		pfadObjekteLaden("Ohne");
+		p = new Primaer();
+		p.neuaufbau();
+	}
+	
+	public void neuaufbau()
+	{
+// Drawing als JComponent mit Strichen und Punkten erstellen und als GlassPane festlegen
+		Drawing linienUndPunkte = new Drawing();
+		linienUndPunkte.WertUebergeben(Sammlung);
+		this.setGlassPane(linienUndPunkte);
+		linienUndPunkte.setVisible(true);
+// Fenster ausführen
+		this.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		 if(e.getSource() == this.buttonSetStart)
+		 {
+			 
+		 }
+		 else if(e.getSource() == this.buttonSetEnd)
+		 {
+			 
+		 }
+		 else if (e.getSource() == this.buttonGetWay)
+		 {
+			 pfadObjekteLaden(comboBoxAttributwahl.getSelectedItem().toString());
+			 this.neuaufbau();
+		 }
+	}
+	
 	public static void pfadObjekteLaden(String aMerkmal)
 	{
-		String csvDatenbasis = ".\\DatenBasis.CSV";
+/*
+Füllt das Array "Sammlung" mit Pfad-Objekten des gewählten Merkmals
+Quelle ist die angegebene CSV Datei
+ */
 		//Sammlung=null;
 		
-		int spalte_StartPX=3;
-		int spalte_StartPY=4;
-		int spalte_EndPX=5;
-		int spalte_EndPY=6;
-		int spalte_MerkmalBarrierefrei=8;
+		
 		
 		int aktivesMerkmal= 0;
 		
@@ -72,7 +192,6 @@ public class Primaer
 			aktivesMerkmal=0;
 		}
 		
-		
 		BufferedReader br = null;
         String line = "";
         int Zaehler = 0;
@@ -85,12 +204,12 @@ public class Primaer
             while ((line = br.readLine()) != null)
             {
                 String[] eintragGelesen = line.split(";");
-                System.out.println(eintragGelesen[aktivesMerkmal]);
+                //System.out.println(eintragGelesen[aktivesMerkmal]);
                 if ((aktivesMerkmal==0)||(Integer.parseInt(eintragGelesen[aktivesMerkmal])==1))
                 {
                 	Sammlung = Arrays.copyOf(Sammlung, 1+Zaehler);
                 	Sammlung[Zaehler] = new Pfad(Integer.parseInt(eintragGelesen[spalte_StartPX]),Integer.parseInt(eintragGelesen[spalte_StartPY]),Integer.parseInt(eintragGelesen[spalte_EndPX]),Integer.parseInt(eintragGelesen[spalte_EndPY]));
-            		System.out.println(Zaehler);
+            		//System.out.println(Zaehler);
                 	Zaehler++;
                 }
                 
@@ -116,6 +235,90 @@ public class Primaer
                 }
             }
         }		
+	}
+	
+	public String naechstenPunktErmitteln(int xSuche, int ySuche)
+	{
+		BufferedReader br = null;
+        String line = "";
+        //int Zaehler=0;
+        double minDist = 100000;
+        String bestYet = "";
+        try
+        {
+            br = new BufferedReader(new FileReader(csvDatenbasis));
+            br.readLine();
+            while ((line = br.readLine()) != null)
+            {
+                String[] eintragGelesen = line.split(";");
+                //System.out.println(eintragGelesen[aktivesMerkmal]);
+                if (Math.sqrt(Math.pow((Integer.parseInt(eintragGelesen[spalte_StartPX])-xSuche),2)+Math.pow((Integer.parseInt(eintragGelesen[spalte_StartPY])-ySuche),2))<minDist)
+                {
+                	
+                	minDist=Math.sqrt(Math.pow((Integer.parseInt(eintragGelesen[spalte_StartPX])-xSuche),2)+Math.pow((Integer.parseInt(eintragGelesen[spalte_StartPY])-ySuche),2));
+                	bestYet=eintragGelesen[spalte_NamePStart];
+                }
+                if (Math.sqrt(Math.pow((Integer.parseInt(eintragGelesen[spalte_EndPX])-xSuche),2)+Math.pow((Integer.parseInt(eintragGelesen[spalte_EndPY])-ySuche),2))<minDist)
+                {
+                	minDist=Math.sqrt(Math.pow((Integer.parseInt(eintragGelesen[spalte_EndPX])-xSuche),2)+Math.pow((Integer.parseInt(eintragGelesen[spalte_EndPY])-ySuche),2));
+                	bestYet=eintragGelesen[spalte_NamePEnd];
+                }
+            }
+
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } finally
+        {
+            if (br != null)
+            {
+                try
+                {
+                    br.close();
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    return bestYet;
+	}
+	
+	class ClickListener extends MouseAdapter
+	{	
+		public void mouseClicked(MouseEvent e)
+		{
+			maybeShowPopup(e);
+		}
+		public void mouseEntered(MouseEvent e)
+		{
+			//maybeShowPopup(e);
+		}
+		public void mouseExited(MouseEvent e)
+		{
+			//maybeShowPopup(e);
+		}
+		public void mousePressed(MouseEvent e)
+		{
+			//maybeShowPopup(e);
+		}
+		public void mouseReleased(MouseEvent e)
+		{
+			maybeShowPopup(e);
+		}
+		public void maybeShowPopup(MouseEvent e)
+		{
+			if (e.isPopupTrigger())
+			{
+				popup.show(Primaer.this, e.getX(), e.getY());
+				MenuXPos=e.getX();
+				MenuYPos=e.getY();
+				//System.out.println("XPos: "+MenuXPos+"  YPos: "+MenuYPos);
+			}
+		}
 	}
 
 }
